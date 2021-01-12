fef86bd5e35240b7cfa4a5bc5bf913f11dd48ff8
PabloDreams <pablo@dreamsengine.io>
2021-01-11 23:06:50 -0600

    first

---
+++ index.php
@@ -0,0 +1,282 @@
+<?php get_header(); ?>
+
+<section class="hero">
+    <div class="hero_inf">
+        <h2>Workshop and Master Class with</h2>
+        <h1>ENRIQUE ARTURO DIEMECKE</h1>
+        <h3>and the Carlos Chávez Symphony Orchestra in historic Chapultepec Park in Mexico City</h3>
+        <h4>August 30 – September 12, 2021</h4>
+    </div>
+</section>
+
+<section class="desc">
+    <h2>Maestro	Diemecke</h2>
+    <p>Artistic General Director of the famed Teatro Colón in Buenos Aires and award-winning Music Director of the Buenos Aires Philharmonic and the Flint Symphony Orchestra in Michigan, will select 8 aspiring conductors and 10 auditors for the intensive two-week program focusing on repertoire of the 19thcentury.  The Xirst week’s activities will be an unprecedented week of remote webinar score analysis led by Maestro Diemecke, followed by one week of rehearsals and concerts in Mexico City by the participants with the Carlos Chavez Orchestra in the Centro Cultural Los Pinos. </p>
+
+    <div class="desc_detail">
+        <div class="desc_detail_left">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/photo.jpg" alt="amproclasica">
+        </div>
+        <div class="desc_detail_right">
+            <h4>Application	Deadline:</h4>
+            <p>June	1,	2021</p>
+            <h4>Webinar	Dates:</h4>
+            <p>	August 30 – September 3, 2021</p>
+            <h4>Rehearsals in Mexico City:</h4>
+            <p>	September 6 – 10, 2021 </p>
+            <h4>Concert:</h4>
+            <p>September 11, 2021</p>
+        </div>
+    </div>
+</section>
+
+<div class="parallax0 parallax">
+    <div class="parallax_content"></div>
+</div>
+
+<section class="schedule">
+    <p>In 2021 Maestro Enrique Diemecke will present master classes and workshops for orchestral conductors with the Orquesta Escuela Carlos Chávez of the Sistema Nacional de Fomento Musical of Mexico, focussing on orchestral romantic repertoire of the 19th Century, including works of Beethoven, Brahms, Wagner, Mendelssohn, Tchaikovsky, Dvorak, Sibelius, and Grieg.</p> 
+
+    <div class="program_detail">
+        <h2>Program details</h2>
+        <p>Eight aspiring conductors will be selected as Full Participanting conductors in workshops and master clases with two pianos, rehearsals with the orchestra, one dress rehearsal and one public concert in the new Centro Cultural Los Pinos, located in the beautiful, historic Chapultepec Park in Mexico City. (September 6 to September 12, 2021.)</p>
+
+        <p>In advance of the workshops and master clases, Maestro Diemecke will prepare the orchestra for one week in sectional rehearsals (August 23 to 29.) The following week he will rehearse the full orchestra and lead them in a concert of the repertoire selected for the master clases (August 30 – September 5.)</p>
+
+        <p>The podium time for each Full Participating conductor will be as follows:</p>
+
+        <ul>
+            <li>Workshops with two pianos, totalling 55 minutes.</li>
+            <li>Rehearsals with orchestra, totalling 75 minutes.</li>
+            <li>Dress rehearsal, 11 minutes.</li>
+            <li>Concert, work of 10 minutes to be assigned by Maestro Diemecke from the list below.</li>
+        </ul>
+
+        <p>Previous to the workshops and master classes in Mexico City, the Full Participanting conductors will receive a 3 hour daily webinar session with Maestro Diemecke (from August 30th to September 3rd), in preparation of the live sessions. </p>
+
+        <p>Tuition for Full Participanting conductors includes shared hotel accommodations in a double room, breakfast for 8 days, and local transportation to and from all sessions.</p>
+
+        <p>In addition to the 8 Full Participanting conductors, there will be 10 Auditor conductors selected, who will attend all of the sessions with the Full Participating conductors. In addition, Auditor conductors will have two 15 minute sessions with two pianos.</p>
+
+        <p>All sessions will be professionally recorded by the organizer and offered to the participants.</p>
+    </div>
+</section>
+
+<div class="parallax1 parallax">
+    <div class="parallax_content1"></div>
+</div>
+
+
+<section class="desc">
+    <h2>Recruitment</h2>
+    <p>The organizers will advertise internationally for participants. Applicants will submit cvs, letters of reccomendation and video recordings. Participants and auditors will be selected by Maestro Diemecke.</p>
+
+    <div class="desc_detail">
+        <div class="desc_detail_left">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/Diemecke_13.jpg" alt="amproclasica">
+        </div>
+        <div class="desc_detail_right">
+            <h3>Fees</h3>
+            <ul>
+                <li>Full Participanting conductors: USD $3,700</li>
+                <li>Auditor conductors: USD $350</li>
+                <li>Application fee (not refundable): USD $65</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="desc_detail">
+        <div class="desc_detail_left">
+            <h3>Focus of the Sessions</h3>
+            <ul>
+                <li>Preparation and Score Analysis.</li>
+                <li>Baton Technique.</li>
+                <li>Body Expression and Communication.</li>
+                <li>Preparation and Rehearsal Technique.</li>
+                <li>Preparing a Season (Programming).</li>
+                <li>Performance Practices.</li>
+            </ul>  
+        </div>
+        <div class="desc_detail_right">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/session.jpg" alt="amproclasica">
+        </div>
+    </div>
+</section>
+
+<section class="schedule">
+    <div class="repertorie">
+        <h2>Repertoire</h2>
+        <div class="repertorie_desc">
+            <div>
+                <p><strong>Beethoven</strong> <i>Egmont Overture</i> 9’ </p>
+                <p><strong>Brahms</strong> <i>Academic Festival Overture</i> 10’ </p>
+                <p><strong>Dvorak</strong> <i>Carnival Overture</i> 9’ </p>
+                <p><strong>Grieg</strong> <i>Peer Gynt t, selections (Morning Mood, The Death of Ase, In the hall of the Mountain King)</i> 10’ </p>
+                <p><strong>Mendelssohn</strong> <i>The Hebrides</i> 11’ </p>
+                <p><strong>Tchaikovsky</strong> <i> Waltzes from The Nutcracker or Swan Lake</i> 9’ </p>
+                <p><strong>Wagner</strong> <i>The Flying Dutchman, Overture</i> 10’ </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="parallax2 parallax">
+    <div class="parallax_content1"></div>
+</div>
+
+<section class="desc">
+    <h2>Dates of the Program</h2>
+    <p>-Week No. 1, Orchestral sectionals with Maestro Diemecke (August 23 to 29.)</p>
+    <p>-Week No. 2, Rehearsals and Concert with Maestro Diemecke (August 30 – September 5.)Webinars with the Full Participating conductors (August 30 to September 3).</p>
+    <p>-Week No. 3, Master Classes, Workshops and Concert (September 6 to 12).</p>
+</section>
+
+<div class="parallax3 parallax">
+    <div class="parallax_content"></div>
+</div>
+
+<section class="schedule">
+    <h2>Administration team</h2>
+    <p><strong>Enrique Arturo Diemecke</strong>, Artistic Director and Master Class Director </p>
+    <p><strong>Benito Alcocer</strong>, Operations Director, Assistant to Artistic Director</p>
+    <p><strong>Isabel Wolf,</strong>, Admissions and International Affairs</p>
+
+    <h4>Enrique Arturo Diemecke, Music Director</h4>
+    <ul>
+        <li>Maestro Diemecke has been a teacher of the leading Mexican conductors of recent years, including: Jesús Medina, Director of the Jalisco Philharmonic; Carlos M. Prieto, Director of the Louisville Orchestra and the National Symphony Orchestra of México; Juan Carlos Lomónaco, Director of the Yucatán Orchestra; Alondra de la Parra, former Director of the Queensland Symphony; Christian Gohmer, Director of Solistas Ensamble Choir of the National Fine Arts Institute of Mexico (INBA); Rodrigo Sierra Moncayo, Director of Zapopan Youth Orchestra of Jalisco; Abdiel Vázquez, pianist and Artistic Director of Opera of Morelia.</li>
+        <li>From 2016 to 2019 Maestro Diemecke received the financial support “Creadores escénicos con trayectoria” from the Federal Culture Secretary of México to give Master Classes and Workshops throughout the country to more than 70 conducting students, culminating in Maestro Diemecke’s forthcoming book of his experiences as conductor and teacher.</li>
+        <li>From 2018 Maestro Diemecke has been the head of the Seminars of Orquestal Conducting of the prestigious Teatro Colón of Buenos Aires, Argentina, belonging to the Superior Art Institute and the Academic Orchestra.</li>
+        <li>From 2007 to 2011, Maestro Diemecke was both teacher of Music Direction and Music Director of the Academic Orchestra of the Superior School of Music of the National Fine Arts Institute of Mexico.</li>
+        <li>During the 2020 quarantine, Diemecke was invited to give two webinar conducting Master Classes for the Seminar “Eduardo Mata” of the National University Autonomous of Mexico (UNAM).</li>
+    </ul>
+</section>
+
+<div class="parallax4 parallax">
+    <div class="parallax_content1"></div>    
+</div>
+
+<section class="desc">
+    <h2>Schedule / Week No. 3</h2>
+
+    <div style="overflow-x:auto;">
+        <table>
+            <tr>
+                <th>SCHEDULE</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+                <th>Monday</th>
+            </tr>
+            <tr>
+                <th>09:00-12:00</th>
+                <th>Welcome / Introduction</th>
+                <th>Workshops / 2 pianos</th>
+                <th>Workshops / 2 pianos</th>
+                <th>Workshops / 2 pianos</th>
+                <th>Workshops / 2 pianos</th>
+                <th>Feedback</th>
+                <th></th>
+                <th>Feedback</th>
+            </tr>
+            <tr>
+                <th>10:00-13:30</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th>Dress rehearsal</th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr>
+                <th>12:00-14:00</th>
+                <th>Lunch Break</th>
+                <th>Lunch Break</th>
+                <th>Lunch Break</th>
+                <th>Lunch Break</th>
+                <th>Lunch Break</th>
+                <th>Lunch Break / Feedback</th>
+                <th>Lunch Break / Feedback</th>
+                <th>Lunch Break / Feedback</th>
+            </tr>
+            <tr>
+                <th>14:00-17:30</th>
+                <th>Workshops / 2 pianos</th>
+                <th>Rehearsal with orchestra</th>
+                <th>Rehearsal with orchestra</th>
+                <th>Rehearsal with orchestra</th>
+                <th>Rehearsal with orchestra</th>
+                <th></th>
+                <th>Concert</th>
+                <th>Closing Ceremony. </th>
+            </tr>
+            <tr>
+                <th>18:00-20:00</th>
+                <th></th>
+                <th>Feedback</th>
+                <th>Feedback</th>
+                <th>Feedback</th>
+                <th>Feedback</th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </table>
+    </div>
+</section>
+
+<section class="schedule">
+    <h2>Contact</h2>
+    <h3>Enrique Arturo Diemecke</h3>
+    <p>eadiemecke@gmail.com</p>
+    <h3>Master Class 2021</h3>
+    <p>www.amproclasica.com</p>
+    <h3>Amproclasica</h3>
+    <p>informes@amproclasica.com</p>
+    <p>benito.alcocer@amproclasica.com</p>
+
+</section>
+
+<style>
+  .hero {
+    background-image: url("<?php echo get_template_directory_uri(); ?>/img/hero.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .parallax0{
+    background-image: url("<?php echo get_template_directory_uri(); ?>/img/violin.jpg");
+  }
+
+  .parallax1{
+    background-image: url("<?php echo get_template_directory_uri(); ?>/img/piano.jpg");
+  }
+
+  .parallax2{
+    background-image: url("<?php echo get_template_directory_uri(); ?>/img/cello.jpg");
+  }
+
+  .parallax3{
+    background-image: url("<?php echo get_template_directory_uri(); ?>/img/Diemecke_12.jpg");
+  }
+
+  .parallax4{
+    background-image: url("<?php echo get_template_directory_uri(); ?>/img/Diemecke_17.jpg");
+  }
+
+  .parallax {
+  height: 300px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+
+<?php get_footer(); ?>
